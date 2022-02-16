4e0d36c5e4cf452795bc95df84f9611d94bded37
Firdosh Ansari <firdosh.ansari0202@gmail.com>
2022-02-16 19:24:45 +0530

    Unique finder using XOR

--- com/firdosh/Bitwise.java
+++ com/firdosh/Bitwise.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Bitwise {
     public static void main(String[] args) {
+
+        int[] arr = {2,4,5,2,4,5,6};
+        System.out.println(unique(arr));
+
         System.out.println("Enter a number");
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
@@ -26,4 +30,13 @@ public class Bitwise {
 
 
     }
+
+    private static int unique(int[] arr) {
+        int unique = 0;
+        for(int n : arr)
+            unique ^= n;
+        return unique;
+    }
+
+
 }
