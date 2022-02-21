2ace2e706c5c551531a872f3b1a0736b92b2173e
Firdosh Ansari <firdosh.ansari0202@gmail.com>
2022-02-21 21:28:30 +0530

    Find foor and ceiling value in Binary search

---
+++ com/firdosh/BSFloorCeilingValue.java
@@ -0,0 +1,39 @@
+package com.firdosh;
+
+public class BSFloorCeilingValue {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,7,8,22,33,44,55,66};
+        System.out.println(floor(arr,77));
+        System.out.println(ceiling(arr,77));
+    }
+
+    static int floor(int[] arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] < target){
+                start = mid + 1;
+            }else if (arr[mid] > target) end = mid - 1;
+            else return arr[mid];
+        }
+        return arr[end % arr.length];
+    }
+
+    static int ceiling(int[] arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] < target){
+                start = mid + 1;
+            }else if (arr[mid] > target) end = mid - 1;
+            else return arr[mid];
+        }
+        return arr[start % arr.length];
+    }
+}
